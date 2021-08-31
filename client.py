d7265c82078bbb58d78659c22ed6af0bb8fba11a
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2021-08-31 14:12:09 -0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,54 @@
+import socket,os,time,math,sys
+from tkinter import *
+from tkinter import ttk
+from hurry.filesize import size
+import zipfile
+ip = '192.168.1.2'
+port = 52000
+class client:
+    def __init__(self,ip,port,root):
+        self.root = root
+        #connect
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((ip, port))
+        #ui
+        self.file_list = Listbox(self.root, height=8, width=50, border=0)
+        self.file_list.pack(anchor = "nw")
+        self.file_list.bind('<<ListboxSelect>>', self.select_file)
+        rec = self.s.recv(1024).decode()
+        Button(self.root, text="Download", bd = 0, font = ("calibri", 15), command=self.download).pack()
+        self.rec = rec.split(",")
+        for i in self.rec:
+            self.file_list.insert(END, (i))
+    def select_file(self,event):
+        #selects the file and gets the index
+        self.index = self.file_list.curselection()[0]
+        
+    def download(self):
+        file = self.rec[self.index]
+        self.s.send(file.encode())
+        file_size = self.s.recv(1024).decode()
+        is_dir = self.s.recv(1024).decode()
+        is_dir = True if is_dir == "0" else False
+        if is_dir == True:
+            file_1 = file + ".zip"
+        else:
+            file_1 = file
+        jsonString = bytearray()
+        x = 0
+        while True:
+            packet = self.s.recv(1024)
+            x += 1024
+            if not packet:
+                break
+            jsonString.extend(packet)
+        with open(file_1, "wb+") as w:
+            w.write(jsonString)
+        if is_dir == True:
+            with zipfile.ZipFile(file_1, 'r') as my_zip:
+                my_zip.extractall(file)
+            os.remove(file_1)
+                
+root = Tk()
+app = client(ip,port,root)
+mainloop()
---
+++ server.py
@@ -0,0 +1,43 @@
+import socket,os,shutil
+from zipfile import ZipFile
+ip = '192.168.1.2'
+port = 52000
+class server:
+    def __init__(self,ip,port):
+        # sets up the server
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((ip, port))
+        s.listen(5)
+        self.conn, addr = s.accept()
+        # gets the list of files to send to the client
+        files = os.listdir()
+        x = ""
+        for i in files:
+            x += i + ","
+        self.conn.send(x.encode())
+        # receives the name of the file and sends back the size of it
+        file = self.conn.recv(1024).decode()
+        self.file_size = os.path.getsize(file)
+        self.conn.send(str(self.file_size).encode())
+        # checks if is it folder or a file
+        a = os.path.isdir(file)
+        is_dir = "0" if a == True else "1" 
+        # if its a folder it makes the folder a zip file to send it
+        if is_dir == "0":
+            shutil.make_archive(file, 'zip',file)
+            file += ".zip"
+        # sends is it a folder or a file to the client
+        self.conn.send(is_dir.encode())
+        self.download(file)
+        os.remove(file)
+    def download(self,file):
+        #reads the file by 1024 and sends it to the client
+        with open(file,"rb") as r:
+            while True:
+                data = r.read(1024)
+                if not data:break
+                self.conn.send(data)
+
+
+app = server(ip,port)
