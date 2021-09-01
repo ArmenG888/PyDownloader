db4724b72c1c8beca1cf633c2e57b9c7a6329949
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2021-09-01 16:10:19 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,33 @@
+from tkinter import *
+from tkinter import filedialog,messagebox
 import socket,os,shutil
 from zipfile import ZipFile
 ip = '127.0.0.1'
 port = 52000
 class server:
     def __init__(self,ip,port):
-        # sets up the server
+        # set up the server and accept the client
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((ip, port))
         s.listen(5)
         self.conn, addr = s.accept()
+        # choose the directory for the client to download
+        folder_selected = filedialog.askdirectory()
+        self.prepare_download(folder_selected)
+
+    def prepare_download(self,directory=None):
+
         # gets the list of files to send to the client
-        files = os.listdir()
+        files = os.listdir(directory)
+        os.chdir(directory)
         x = ""
         for i in files:
             x += i + ","
         self.conn.send(x.encode())
         # receives the name of the file and sends back the size of it
         file = self.conn.recv(1024).decode()
+
         self.file_size = os.path.getsize(file)
         self.conn.send(str(self.file_size).encode())
         # checks if is it folder or a file
@@ -27,10 +37,12 @@ class server:
         if is_dir == "0":
             shutil.make_archive(file, 'zip',file)
             file += ".zip"
+
         # sends is it a folder or a file to the client
         self.conn.send(is_dir.encode())
         self.download(file)
-        os.remove(file)
+        if is_dir == "0": os.remove(file)
+        #self.root.quit()
     def download(self,file):
         #reads the file by 1024 and sends it to the client
         with open(file,"rb") as r:
@@ -38,6 +50,5 @@ class server:
                 data = r.read(1024)
                 if not data:break
                 self.conn.send(data)
-
 
 app = server(ip,port)
