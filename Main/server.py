0362d897386dbb2aceb3eaf7d46fc73524d47f1a
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2021-09-05 10:01:36 -0700

    Update server.py

--- Main/server.py
+++ Main/server.py
@@ -3,7 +3,7 @@ from tkinter import filedialog,messagebox
 import socket,os,shutil
 from zipfile import ZipFile
 # Detects the ipv4 address to use to bind the server
-ip = socket.gethostbyname(socket.gethostname())
+ip = "127.0.0.1"
 port = 52000
 print("The server is running on IP", ip)
 class server:
